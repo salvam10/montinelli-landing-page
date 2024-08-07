5668a8404456fee71afd59e708eb47b9ea6325e9
salvam10 <smachta1@gmail.com>
2024-08-07 16:22:32 -0400

    d

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const reactRouterMiddleware = (req, res, next) => {
 };
  */
 // Aplica el middleware para las rutas manejadas por React Router
-app.use(reactRouterMiddleware);
+/* app.use(reactRouterMiddleware); */
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
