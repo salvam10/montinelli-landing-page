c1f774768c76706bc2045ed8ce87057bb7ff449a
salvam10 <smachta1@gmail.com>
2024-08-07 16:17:12 -0400

    without routes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var SQLiteStore = require("connect-sqlite3")(session);
 
 /* inicializar enrutadores */
 
-const productVariationAttsRouter = require("./routes/prodVariationAttributes");
+/* const productVariationAttsRouter = require("./routes/prodVariationAttributes");
 const productVariationsRouter = require("./routes/product_variations");
 const attributesValuesRouter = require("./routes/attributesValues");
 const categoriesRouter = require("./routes/product_categories");
@@ -26,14 +26,14 @@ const clientsRouter = require("./routes/clients");
 const ordersRouter = require("./routes/orders")
 const cartsRouter = require("./routes/carts");
 const usersRouter = require("./routes/users");
-const authRouter = require("./routes/auth");
+const authRouter = require("./routes/auth"); */
 
 const CLIENT_URL = process.env.CLIENT_URL;
 
 /* React como motor de vistas */
 /* app.use(express.static("client/build")); */
 // Define las rutas que deseas que sean manejadas por React Router
-const reactRouterRoutes = [
+/* const reactRouterRoutes = [
   "/",
   "/signin",
   "/categorias",
@@ -44,9 +44,9 @@ const reactRouterRoutes = [
   "/admin",
   "/orders",
   "/orders/:id"
-];
+]; */
 
-console.log('ddd');
+/* console.log('ddd');
 // Middleware para las rutas manejadas por React Router
 const reactRouterMiddleware = (req, res, next) => {
   if (reactRouterRoutes.includes(req.url)) {
@@ -57,7 +57,7 @@ const reactRouterMiddleware = (req, res, next) => {
     next();
   }
 };
-
+ */
 // Aplica el middleware para las rutas manejadas por React Router
 app.use(reactRouterMiddleware);
 
@@ -84,7 +84,7 @@ app.use(
   })
 );
 
-app.use("/api/product-variation-attributes", productVariationAttsRouter);
+/* app.use("/api/product-variation-attributes", productVariationAttsRouter);
 app.use("/api/product-variations", productVariationsRouter);
 app.use("/api/attributes-values", attributesValuesRouter);
 app.use("/api/attributes", attributesRouter);
@@ -95,9 +95,10 @@ app.use("/api/brands", brandsRouter);
 app.use("/api/orders", ordersRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/cart", cartsRouter);
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter); */
 
 app.get("/", async (req, res, next) => {
+  console.log('welcome');
   res.send("Welcome to GSM");
 });
 
