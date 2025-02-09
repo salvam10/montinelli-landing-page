1f174932b6964dbefa300083f48bad6611d5af19
salvam10 <smachta1@gmail.com>
2025-02-09 14:46:14 -0400

    xdd

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const authRouter = require("./routes/auth");
 const CLIENT_URL = process.env.CLIENT_URL;
 
 /* React como motor de vistas */
-/* app.use(express.static("client/build")); */
+app.use(express.static("client/build"));
 // Define las rutas que deseas que sean manejadas por React Router
 
 const reactRouterRoutes = [
